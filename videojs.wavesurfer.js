78b59c4c652633e918e563256b261245473f720d
Girish Kulkarni <girishku@cybage.com>
2015-08-26 19:28:22 +0530

    Define custom Container to plot the waveform

--- videojs.wavesurfer.js
+++ videojs.wavesurfer.js
@@ -198,7 +198,12 @@
             }
 
             // set waveform element and dimensions
-            opts.container = this.el();
+			// Set the container to player's container if "container" option is not provided
+			// If a waveform needs to be appended to your custom element, then use below option
+			// <code>container: document.querySelector("#vjs-waveform")</code>
+            if (opts.container == undefined) {
+				opts.container = this.el();
+			}
             opts.height = this.player().height() - controlBarHeight;
 
             // customize waveform appearance
