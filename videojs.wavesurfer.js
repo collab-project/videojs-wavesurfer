9d4fa31cfa4b1d5102d9267997dace356c90921f
gintelllect <girishku@cybage.com>
2015-08-26 19:36:12 +0530

    Define custom Container to plot the waveform

--- videojs.wavesurfer.js
+++ videojs.wavesurfer.js
@@ -50,25 +50,25 @@
 
             if (this.options().options.msDisplayMax !== undefined)
             {
-                // msDisplayMax indicates the number of seconds that is
+            	// msDisplayMax indicates the number of seconds that is
                 // considered the boundary value for displaying milliseconds
                 // in the time controls. An audio clip with a total length of
                 // 2 seconds and a msDisplayMax of 3 will use the format
                 // M:SS:MMM. Clips longer than msDisplayMax will be displayed
-                // as M:SS or HH:MM:SS.
-                this.msDisplayMax = parseFloat(this.options().options.msDisplayMax);
+            	// as M:SS or HH:MM:SS.
+            	this.msDisplayMax = parseFloat(this.options().options.msDisplayMax);
             }
             else
             {
-                // default
-                this.msDisplayMax = 3;
+            	// default
+            	this.msDisplayMax = 3;
             }
 
             // customize controls
             this.player().bigPlayButton.hide();
             if (this.player().options().controls)
             {
-                // progress control isn't used by this plugin
+            	// progress control isn't used by this plugin
                 this.player().controlBar.progressControl.hide();
 
                 // prevent controlbar fadeout
