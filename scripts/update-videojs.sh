5907bb59a02025a03aacfe23fc5b37b2764d2ed8
Thijs Triemstra <info@collab.nl>
2017-10-03 22:20:05 +0200

    update script

--- scripts/update-videojs.sh
+++ scripts/update-videojs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export OLD_VERSION=5.19.2
-export NEW_VERSION=6.2.8
+export OLD_VERSION=6.2.8
+export NEW_VERSION=6.3.1
 
 # check if language files exist
-wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
+# wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
 
 # update videojs in examples and documentation
 find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
