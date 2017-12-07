5242d497b9bd253b1023452a9c66de3bfc8aa5bb
Michael Fairchild <mfairchild365@gmail.com>
2017-12-07 14:42:22 -0600

    Only modify the html5 tech if the player is using wavesurfer
    This should fix regular video playback.

--- src/js/tech.js
+++ src/js/tech.js
@@ -32,6 +32,11 @@ class WavesurferTech extends Html5 {
             this.waveready = true;
         }.bind(this));
 
+        if (!this.playerIsUsingWavesurfer()) {
+            //The plugin hasn't been initialized for this player, so it likely doesn't need our html5 tech modifications
+            return;
+        }
+
         // proxy timeupdate events so that the tech emits them too. This will
         // allow the rest of videoJS to work (including text tracks)
         this.activePlayer.activeWavesurferPlugin.on('timeupdate', function() {
@@ -39,11 +44,30 @@ class WavesurferTech extends Html5 {
         }.bind(this));
     }
 
+    /**
+     * Determine whether or not the player is trying use wavesurfer
+     * @returns {boolean}
+     */
+    playerIsUsingWavesurfer()
+    {
+        return this.activePlayer.activeWavesurferPlugin !== undefined;
+    }
+
     play() {
+        if (!this.playerIsUsingWavesurfer()) {
+            //fall back to html5 tech functionality
+            return super.play();
+        }
+
         return this.activePlayer.activeWavesurferPlugin.play();
     }
 
     pause() {
+        if (!this.playerIsUsingWavesurfer()) {
+            //fall back to html5 tech functionality
+            return super.pause();
+        }
+
         return this.activePlayer.activeWavesurferPlugin.pause();
     }
 
@@ -52,6 +76,11 @@ class WavesurferTech extends Html5 {
      * @return {number}
      */
     currentTime() {
+        if (!this.playerIsUsingWavesurfer()) {
+            //fall back to html5 tech functionality
+            return super.currentTime();
+        }
+
         if (!this.waveready) {
             return 0;
         }
@@ -66,6 +95,11 @@ class WavesurferTech extends Html5 {
      *         The duration of the media or 0 if there is no duration.
      */
     duration() {
+        if (!this.playerIsUsingWavesurfer()) {
+            //fall back to html5 tech functionality
+            return super.duration();
+        }
+
         if (!this.waveready) {
             return 0;
         }
