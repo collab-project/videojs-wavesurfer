7d3f0f9cba72352a55f02d3a0118350a385d6a0f
Thijs Triemstra <info@collab.nl>
2018-03-26 01:50:08 +0200

    add karma setup for tests

---
+++ karma.conf.js
@@ -0,0 +1,66 @@
+/* eslint-env node */
+
+process.env.BABEL_ENV = 'test';
+
+require('babel-register');
+//var webpackConfig = require('./build-config/webpack.prod.main.js');
+
+module.exports = function(config) {
+    var configuration = {
+        basePath: '',
+        frameworks: ['jasmine', 'jasmine-matchers'],
+        hostname: 'localhost',
+        port: 9876,
+        singleRun: true,
+        autoWatch: false,
+        files: [
+            // demo audio file
+            {
+                pattern: 'test/spec/support/demo.wav',
+                included: false,
+                watched: false,
+                served: true
+            },
+
+            // specs
+            'test/spec/util.spec.js',
+        ],
+        preprocessors: {
+            'test/spec/util.spec.js': ['webpack'],
+            // source files, that you want to generate coverage for
+            // do not include tests or libraries
+            'src/js/**/*.js': ['coverage']
+        },
+        webpackMiddleware: {
+            stats: 'errors-only'
+        },
+        plugins: [
+            'karma-webpack',
+            'karma-jasmine',
+            'karma-jasmine-matchers',
+            'karma-chrome-launcher',
+            'karma-coverage'
+        ],
+        browsers: ['Chrome'],
+        captureConsole: true,
+        colors: true,
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
+        //webpack: webpackConfig,
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
+};
---
+++ test/spec/util.spec.js
@@ -0,0 +1,12 @@
+/* eslint-env jasmine */
+
+import pluginDefaultOptions from '../../es5/defaults.js';
+
+/** @test {defaults} */
+describe('defaults:', function() {
+
+    /** @test {pluginDefaultOptions} */
+    it('pluginDefaultOptions returns a non-empty object', function() {
+        expect(pluginDefaultOptions).toBeNonEmptyObject();
+    });
+});
