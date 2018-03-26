103d359ff706b33a990e8c0769be7067625af766
Thijs Triemstra <info@collab.nl>
2018-03-26 22:11:21 +0200

    add tests for formatTime

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -6,9 +6,43 @@ import Wavesurfer from '../src/js/videojs.wavesurfer.js';
 describe('format-time:', function() {
 
     /** @test {formatTime} */
-    it('formatTime returns a string', function() {
+    it('formatTime returns a formatted string for seconds', function() {
+        let time = formatTime(10);
+        expect(time).toEqual('0:10');
+
+        time = formatTime(11);
+        expect(time).toEqual('0:11');
+
+        time = formatTime(121);
+        expect(time).toEqual('2:01');
+
+        time = formatTime(3661);
+        expect(time).toEqual('1:01:01');
+    });
+
+    /** @test {formatTime} */
+    it('formatTime returns a formatted string when using msDisplayMax', function() {
+        let time = formatTime(2.011, 3, 10);
+        expect(time).toEqual('0:02:011');
+
+        time = formatTime(4.121, 3, 10);
+        expect(time).toEqual('0:04:121');
+
+        time = formatTime(6.001, 2, 20);
+        expect(time).toEqual('0:06:001');
+    });
+
+    /** @test {formatTime} */
+    it('formatTime returns a string when no arguments are received', function() {
         let time = formatTime();
 
         expect(time).toEqual('-:-');
+    });
+
+    /** @test {formatTime} */
+    it('formatTime defaults to 0 when a negative value is received', function() {
+        let time = formatTime(-2);
+
+        expect(time).toEqual('0:00');
     });
 });
