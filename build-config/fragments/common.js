4600d9da33f059636dd54dc3eab1bdce97912df6
Thijs Triemstra <info@collab.nl>
2018-05-15 14:18:04 +0200

    use webpack banner plugin for css

--- build-config/fragments/common.js
+++ build-config/fragments/common.js
@@ -7,33 +7,36 @@ const path = require('path');
 const webpack = require('webpack');
 const moment = require('moment');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 
 const time = moment().format('YYYY');
 const pckg = require(path.join(__dirname, '..', '..', 'package.json'));
 
-// add banner with copyright and version info
-var banner = `${pckg.name}
+// add JS banner with copyright and version info
+var jsBanner = `${pckg.name}
 @version ${pckg.version}
 @see ${pckg.homepage}
 @copyright 2014-${time} ${pckg.author}
 @license ${pckg.license}`;
-var bannerPlugin = new webpack.BannerPlugin({
-    banner: banner,
+var jsBannerPlugin = new webpack.BannerPlugin({
+    banner: jsBanner,
     test: /\.js$/
 });
-// inject version number
-var replaceVersionPlugin = new webpack.DefinePlugin({
+
+// add CSS banner with version info
+var cssBanner = `/*!
+Default styles for ${pckg.name} ${pckg.version}
+*/`;
+var cssBannerPlugin = new webpack.BannerPlugin({
+    banner: cssBanner,
+    raw: true,
+    test: /\.css$/
+});
+
+// inject JS version number
+var jsVersionPlugin = new webpack.DefinePlugin({
   '__VERSION__': JSON.stringify(pckg.version)
 });
-var cssVersionPlugin = new ReplaceInFileWebpackPlugin([{
-    dir: 'dist/css',
-    test: [/\.css$/],
-    rules: [{
-        search: /LIB_VERSION/ig,
-        replace: pckg.version
-    }]
-}]);
+
 var rootDir = path.resolve(__dirname, '..', '..');
 
 module.exports = {
@@ -74,8 +77,8 @@ module.exports = {
         ]
     },
     plugins: [
-        bannerPlugin,
-        replaceVersionPlugin,
-        cssVersionPlugin
+        jsBannerPlugin,
+        jsVersionPlugin,
+        cssBannerPlugin
     ]
 };
