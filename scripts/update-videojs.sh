e1c218dcc921feb374516b203e23a1960df239fc
Thijs Triemstra <info@collab.nl>
2018-07-17 00:51:54 +0200

    update to video.js 7.0.5 in examples

--- scripts/update-videojs.sh
+++ scripts/update-videojs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-export OLD_VERSION=6.9.0
-export NEW_VERSION=7.0.3
+export OLD_VERSION=7.0.3
+export NEW_VERSION=7.0.5
 
 # check if language files exist
 # wget --delete-after https://vjs.zencdn.net/$NEW_VERSION/lang/nl.js
 
 # update videojs in examples and documentation
-find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
+if [ "$(uname)" == "Darwin" ]; then
+    find . -type f -name '*.html' -exec sed -i '' s/$OLD_VERSION/$NEW_VERSION/ {} +
+else
+    find examples/ -type f -name '*.html' -exec sed -i "s/$OLD_VERSION/$NEW_VERSION/" {} \;
+fi
 
 #git commit -am "bump videojs to $NEW_VERSION in examples"
