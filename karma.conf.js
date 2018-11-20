441b0d46a7f8cc24d9e30cb1cf75cf60f389e716
Thijs Triemstra <info@collab.nl>
2018-11-20 19:42:50 +0100

    fix typo

--- karma.conf.js
+++ karma.conf.js
@@ -115,6 +115,7 @@ module.exports = function(config) {
                     }
                     return result;
                 }
+            }
         },
         customLaunchers: {
             Chrome_dev: {
