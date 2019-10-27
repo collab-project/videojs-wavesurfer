1ace727ca506c102b61f030960b3621bdf15cc03
Thijs Triemstra <info@collab.nl>
2019-10-28 00:53:47 +0100

    media element is default backend

--- examples/react/index.js
+++ examples/react/index.js
@@ -59,7 +59,6 @@ const videoJsOptions = {
     height: 300,
     plugins: {
         wavesurfer: {
-            backend: 'MediaElement',
             msDisplayMax: 10,
             debug: true,
             waveColor: 'white',
