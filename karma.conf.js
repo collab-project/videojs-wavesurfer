d487f9536bdf6b5b2810e817d0841b8e33d6ba13
Thijs Triemstra <info@collab.nl>
2020-10-31 12:57:38 +0100

    fix typo

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ let fakeAudioStream = path.join(support_dir, 'demo.wav');
 // http://peter.sh/experiments/chromium-command-line-switches/
 let chromeFlags = [
     '--no-sandbox',
+    '--disable-gpu',
     '--no-first-run',
     '--noerrdialogs',
     '--no-default-browser-check',
@@ -172,7 +173,7 @@ module.exports = function(config) {
     };
 
     if (ci) {
-        configuration.browsers = ['Firefox_headless', 'Chrome_Headless'];
+        configuration.browsers = ['Firefox_headless', 'Chrome_headless'];
         configuration.detectBrowsers.enabled = false;
         configuration.singleRun = true;
 
